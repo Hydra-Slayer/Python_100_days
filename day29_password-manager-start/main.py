7c850a6fcbcf13a8921d867aec7b4a066eb4fdb9
Hydra-Slayer <shreyassandeep.kodte2021@vitstudent.ac.in>
2024-09-17 19:25:57 +0530

    days 30 error handling commit

---
+++ day29_password-manager-start/main.py
@@ -0,0 +1,75 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+WINDOW_DIM = 200
+EN_WID = 35
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+ 
+label_website=Label(text="Website:")
+label_website.grid(column=0, row=1)
+label_email_uname = Label(text="Email/Username:")
+label_email_uname.grid(column=0, row=2)
+label_password = Label(text="Password:")
+label_password.grid(column=0, row=3)
+ 
+entry_website = Entry()
+entry_email_uname = Entry()
+entry_password = Entry()
+entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
+entry_website.focus()
+entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW", ipadx=10)
+entry_email_uname.insert(END, "email@email.com")
+entry_password.grid(column=1, row=3, sticky="EW")
+
+
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_password():
+    website = entry_website.get()
+    email = entry_email_uname.get()
+    password = entry_password.get()
+    with open("data.txt","a") as data:
+        data.write(f"{website}\n{email}\n{password}\n")
+    entry_website.delete(0,END)
+    entry_password.delete(0,END)
+    messagebox.showinfo(title="Success", message="Password saved succesfully")
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+generate_btn = Button(text="Generate Password", command=generate_password)
+generate_btn.grid(column=2, row=3, sticky="EW")
+add_btn = Button(text="Add", width=35, command=save_password)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+window.mainloop()
+
---
+++ day30_error_handling/main.py
@@ -0,0 +1,28 @@
+# try:
+#     with open("afile.txt") as file:
+#         file.read()
+        
+# except FileNotFoundError as e:
+#     print(f"{e.filename} does not exist.")
+
+# else:
+#     print("Some error occured")
+# finally:
+#     print("Execution completed")
+
+# height = float(input("Enter Height: "))
+# if height> 2:
+#     raise ValueError("Human height cannot be more than 2 meters")
+
+fruits = ["Apple", "Pear", "Orange"]
+
+# Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
+
+try: 
+    make_pie(4)
+
+except (IndexError):
+    print("Fruit pie")
