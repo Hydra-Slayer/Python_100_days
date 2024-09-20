ad3419ea9b02ac7be7721a8e15fa7475c61bfb16
Hydra-Slayer <shreyassandeep.kodte2021@vitstudent.ac.in>
2024-09-21 04:21:55 +0530

    Anki project completed
    Runs on csv files and made in tkinter

---
+++ day31_flashcard_app/main.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+
+
+window = Tk()
+window.title("Anki Clone")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+right_img = PhotoImage(file="./images/right.png")
+wrong_img = PhotoImage(file="./images/wrong.png")
+card_front = PhotoImage(file="./images/card_front.png")
+card_back = PhotoImage(file="./images/card_back.png")
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    og_data = pandas.read_csv("./data/french_words.csv")
+    all_cards = og_data.to_dict(orient='records')
+else:
+    all_cards = data.to_dict(orient='records')
+    
+    
+canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
+
+card = canvas.create_image(400,263, image = card_front)
+lang = canvas.create_text(400,100,text="Language", font=("Ariel", 20))
+words = canvas.create_text(400, 250, text="Word", font=("Ariel", 40))
+
+def flip_card():
+    canvas.itemconfig(card, image = card_back)
+    canvas.itemconfig(words, text = current_card["English"], fill="white")
+    canvas.itemconfig(lang, text = "English", fill = "white")
+
+flip_timer = window.after(3000, func=flip_card)
+
+words_learnt = []
+def learnt():
+    all_cards.remove(current_card)
+    
+    
+    words_learnt.append(current_card)
+    data = pandas.DataFrame(all_cards)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    
+    next_card()
+    
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(card, image = card_front)
+    current_card = random.choice(all_cards)
+    canvas.itemconfig(lang, text = "French", fill="black")
+    canvas.itemconfig(words, text = current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+
+next_card()
+
+canvas.grid(row=0, column=0, columnspan=2, sticky="EW")
+rht_btn = Button(width=100, height=100, image=right_img, command=learnt, highlightthickness=0)
+wrng_btn = Button(width=100, height=100, image=wrong_img, command=next_card, highlightthickness=0)
+rht_btn.grid(row=1, column=0)
+wrng_btn.grid(row=1, column=1)
+
+window.mainloop()
+words_learnt_df = pandas.DataFrame(words_learnt)
+words_learnt_df.to_csv("./data/words_learnt.csv")
